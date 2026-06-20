1ad13bf60ec1cdf7da47b48df6ee74a1f6584d26
higor <higormc2015@gmail.com>
2026-06-20 15:00:45 -0300

    feat: adicionando tela de gerenciar administradores

---
+++ admin/js/administradores.js
@@ -0,0 +1,7 @@
+// Módulos globais do painel
+import './components/AdminSidebar.js';
+import './components/AdminHeader.js';
+
+// Por enquanto, apenas importando a estrutura visual.
+// Lógicas de Mock, modal e eventos serão implementados posteriormente.
+console.log("Interface de Gerenciamento de Administradores carregada com sucesso.");
